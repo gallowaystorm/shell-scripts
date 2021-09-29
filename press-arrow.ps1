e4e3363f861e77d15bbef8839571486d28725fb1
Storm Galloway <storm.galloway@variq.com>
2021-09-29 16:24:16 -0500

    changed seconds

--- press-arrow.ps1
+++ press-arrow.ps1
@@ -3,7 +3,7 @@ Add-Type -AssemblyName System.Windows.Forms
 while ($true)
 {
     [System.Windows.Forms.SendKeys]::SendWait("{PGUP}")
-    Start-Sleep -Seconds 5
+    Start-Sleep -Seconds 10
     [System.Windows.Forms.SendKeys]::SendWait("{PGDN}")
-    Start-Sleep -Seconds 2
+    Start-Sleep -Seconds 20
 }
